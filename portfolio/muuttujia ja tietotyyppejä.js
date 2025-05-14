ee41612fdeb42d635cea1eef20fee1ba193fb5cb
paavoarvonen <pa0645@edu.turku.fi>
2025-05-14 20:42:22 +0300

    toinen javascript tehtävä

---
+++ portfolio/muuttujia ja tietotyyppejä.js
@@ -0,0 +1,20 @@
+console.log("muuttujia ja tietotyyppejä avattu");
+let a = 3;
+let b = 5;
+let c = 7;
+let d = 15;
+function summa() {
+    alert(a + b);
+}
+
+function vähennys() {
+    alert(b - c);
+}
+
+function kerto() {
+    alert(c * d);
+}
+
+function jako() {
+    alert(d / a);
+}
