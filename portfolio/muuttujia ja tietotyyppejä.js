6858933a04f71ed9d502c0175ec434133bf40d47
paavoarvonen <pa0645@edu.turku.fi>
2025-05-27 12:57:04 +0300

    melkee javascript lopus

--- portfolio/muuttujia ja tietotyyppejä.js
+++ portfolio/muuttujia ja tietotyyppejä.js
@@ -24,3 +24,47 @@ function laskin_jako() {
 let b = parseFloat(document.getElementById("2").value) || 0;
     alert(a / b);
 }
+function itseisarvo() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    alert(Math.abs(a));
+}
+function neliöjuuri() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    alert(Math.sqrt(a));
+}
+function potenssi() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(Math.pow(a, b));
+}
+function suurempi() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    let b = parseFloat(document.getElementById("2").value) || 0;
+    if (a > b) {
+        alert(a + " on suurempi kuin " + b);
+    } else if (a < b) {
+        alert(b + " on suurempi kuin " + a);
+    }}
+function pienempi() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    let b = parseFloat(document.getElementById("2").value) || 0;
+    if (a < b) {
+        alert(a + " on pienempi kuin " + b);
+    } else if (a > b) {
+        alert(b + " on pienempi kuin " + a);
+    }}
+function pyöristä() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+    let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(Math.round(a * b));
+}
+
+function päivämäärä() {
+    new Date("27.5.2025");
+    new Date("11.26");
+    new Date("2025-05-27 11.26");
+    new Date("tiistai");
+    new Date("toukokuu");
+}
+function nappula () {
+    alert(päivämäärä);}
